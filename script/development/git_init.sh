52ac82fdaea47ca8aed894b372fd65c10dabe7e4
Samuel Cho <chos5@nku.edu>
2025-07-21 11:45:34 -0400

    update for webhook

---
+++ script/development/git_init.sh
@@ -0,0 +1,3 @@
+git init
+# You should change the repository to yours
+git remote add origin https://github.com/make2grow/my-flask-app
--- script/development/push_code.sh
+++ script/development/push_code.sh
@@ -1,5 +1,3 @@
-#git init
-#git remote add origin https://github.com/make2grow/webserver
 git add .
 git commit -m "Initial commit"
 git push -u origin main
