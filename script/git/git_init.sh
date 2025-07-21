3bc48e8e9c87884c63aec6e10f93655a520d5160
make2grow <make2gr@gmail.com>
2025-07-21 19:45:54 -0400

    Initial commit

---
+++ script/git/git_init.sh
@@ -0,0 +1,5 @@
+git init
+# You should change the repository to yours
+git remote add origin https://github.com/make2grow/my-flask-app
+git pull origin main
+git branch --set-upstream-to=origin/main
--- script/local/git_init.sh
+++
@@ -1,3 +0,0 @@
-git init
-# You should change the repository to yours
-git remote add origin https://github.com/make2grow/my-flask-app
