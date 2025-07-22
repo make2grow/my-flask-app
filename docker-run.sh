53037f5692ee2798102cf6d7b9892d10b3bc8135
Samuel Cho <chos5@nku.edu>
2025-07-22 11:02:39 -0400

    Auto commit: 2025-07-22 11:02:39

--- docker-run.sh
+++ docker-run.sh
@@ -2,8 +2,18 @@
 
 cd /home/deploy/my-flask-app
 
-docker stop flask-running
-docker rm flask-running
+#docker stop flask-running
+#docker rm flask-running
 
-docker build -t flask-app .
-docker run -d --name flask-running --restart=always -p 80:5000 flask-app
+#docker build -t flask-app .
+#docker run -d --name flask-running --restart=always -p 80:5000 flask-app
+
+# Only build if image doesn't exist
+if ! docker image inspect flask-app >/dev/null 2>&1; then
+    docker build -t flask-app .
+fi
+
+# Only run if container doesn't already exist
+if ! docker ps -a --format '{{.Names}}' | grep -q '^flask-running$'; then
+    docker run -d --name flask-running --restart=always -p 80:5000 flask-app
+fi
