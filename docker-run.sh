c11a581a61c37184d340ea3bfb4d35aec269f70b
make2grow <make2gr@gmail.com>
2025-07-21 22:59:15 -0400

    Auto commit: 2025-07-21 22:59:15

--- docker-run.sh
+++ docker-run.sh
@@ -2,9 +2,10 @@
 
 cd /home/deploy/my-flask-app
 
-#docker stop $(docker ps -q) 2>/dev/null
+docker stop $(docker ps -q) 2>/dev/null
 #docker rm $(docker ps -a -q) 2>/dev/null
+docker rm -f flask-app
 rc-service docker restart
 
-docker build -t flask-app .
+#docker build -t flask-app .
 docker run -d --name flask-app --restart=always -p 80:5000 flask-app
