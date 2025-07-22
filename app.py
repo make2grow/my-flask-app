46256fe4831ad4aa676682cb96bc73e5f01821cb
make2grow <make2gr@gmail.com>
2025-07-21 21:12:19 -0400

    Auto commit: 2025-07-21 21:12:19

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Flask!!!!"
+    return "Hello from Flask!"
 
-# 이 부분이 핵심!
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
